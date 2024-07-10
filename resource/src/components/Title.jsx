f859258ec9982c431ae6009d809150521ed8018a
GerardZhou <dorey1200@gmail.com>
2024-07-10 10:25:26 -0500

    upd

--- resource/src/components/Title.jsx
+++ resource/src/components/Title.jsx
@@ -1,4 +1,14 @@
 import styles from "./title.module.css";
 export default function Title() {
-  return <div className={styles.header}>Resources for All AP Classes</div>;
+  return (
+    <div className={styles.header}>
+      <div className={styles.titleContainer}>
+        <div className={styles.title}>Resources for All AP Classes</div>
+      </div>
+      <div className={styles.container}>
+        <button className={styles.missionStatement}>Mission Statement</button>
+        <button className={styles.contact}>Contact</button>
+      </div>
+    </div>
+  );
 }
