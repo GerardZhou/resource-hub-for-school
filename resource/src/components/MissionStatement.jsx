8ff3d5fba81d2b46028ff87d6444c443f2954f3d
GerardZhou <dorey1200@gmail.com>
2024-09-09 18:45:28 -0500

    changes

--- resource/src/components/MissionStatement.jsx
+++ resource/src/components/MissionStatement.jsx
@@ -7,10 +7,9 @@ const MissionStatement = () => {
       <h2 className={styles.heading}>About Us</h2>
       <p className={styles.paragraph}>
         The purpose of this is to help students find resources for APs that are
-        effective for other AP students. This website has a list of easily
-        accessible and free AP Resources for each AP course. If you have any
-        resources that you would like to share, please send us a contact
-        message!
+        worked for other AP students. We provide accessible and free AP
+        Resources for each AP course. If you have any resources that you would
+        like to share, please send us a contact message!
       </p>
     </div>
   );
