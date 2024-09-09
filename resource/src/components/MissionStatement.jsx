613661a354c9d44c50300097f0b75f9e7d5377b8
GerardZhou <dorey1200@gmail.com>
2024-09-09 18:47:18 -0500

    Mission statement message changes

--- resource/src/components/MissionStatement.jsx
+++ resource/src/components/MissionStatement.jsx
@@ -6,7 +6,7 @@ const MissionStatement = () => {
     <div className={styles.missionStatement}>
       <h2 className={styles.heading}>About Us</h2>
       <p className={styles.paragraph}>
-        The purpose of this is to help students find resources for APs that are
+        The purpose of this is to help students find resources for APs that
         worked for other AP students. We provide accessible and free AP
         Resources for each AP course. If you have any resources that you would
         like to share, please send us a contact message!
