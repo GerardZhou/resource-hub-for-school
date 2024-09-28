8b1ba683f30a717169e157449459a6b6868313cb
GerardZhou <dorey1200@gmail.com>
2024-09-28 12:01:11 -0500

    front end changes with resource cards

--- resource/src/components/ResourceCard.jsx
+++ resource/src/components/ResourceCard.jsx
@@ -3,11 +3,12 @@ import Button from "./Button";
 
 export default function ResourceCard({ name, Image, link }) {
   return (
-    <a href={link} className={styles.container}>
-      <img className={styles.image} src={Image} alt={name} />
-      <div>
-        <Button name={name} />
-      </div>
+    <a
+      href={link}
+      className={styles.container}
+      style={{ backgroundImage: `url(${Image})` }} // Set the background image dynamically
+    >
+      <Button name={name} />
     </a>
   );
 }
